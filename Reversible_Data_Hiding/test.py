2c8c8f9d227c0fd49926dc5d2e9623153bda0cae
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2020-01-22 14:25:14 -0600

    modify

--- Reversible_Data_Hiding/test.py
+++ Reversible_Data_Hiding/test.py
@@ -2,14 +2,37 @@ from PIL import Image
 
 import os
 
-root_dir = os.path.dirname(os.getcwd())
-im_path = os.path.join(root_dir, 'Reversible_Data_Hiding/misc/4.1.07.tiff')
 
-im = Image.open(im_path) 
 
-px = im.load()
 
-for i, j in zip(range(0,256), range(0,256)):
-    print (px[i, j])
+
+
+
+
+def get_pixels(image_path):
     
+    # Load pixels from image
+    im = Image.open(image_path) 
+    px = im.load()
 
+    pixels = []
+    for i, j in zip(range(0,256), range(0,256)):
+        pixels.append((px[i, j])) 
+    
+    return pixels
+
+
+def modify_pixels(pixels):
+    pass
+
+
+
+if __name__ == "__main__":
+    # Get the image path
+    root_dir = os.path.dirname(os.getcwd())
+    im_path = os.path.join(root_dir, 'Reversible_Data_Hiding/misc/5.1.09.tiff')
+    
+    pxls = get_pixels(im_path)
+    print(pxls)
+    modify_pixels(pxls)
+    
