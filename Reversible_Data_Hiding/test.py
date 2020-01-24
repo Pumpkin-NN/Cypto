25ae7ce19428bef508876c72877898ac5925cc53
PumpkinQ <55161270+Pumpkin-NN@users.noreply.github.com>
2020-01-24 00:11:12 -0600

    modify

--- Reversible_Data_Hiding/test.py
+++ Reversible_Data_Hiding/test.py
@@ -8,7 +8,7 @@ import os
 
 
 
-
+# Get the pixels from the grey-scale image(256x256)
 def get_pixels(image_path):
     
     # Load pixels from image
@@ -21,9 +21,17 @@ def get_pixels(image_path):
             pixels.append((px[i, j]))
     return pixels
 
+# Find the inverse of the pixels
+def inverse_pixels(pixels):
+    inv_pixels = []
+    for k in pixels:
+        m = (k + 128) % 256
+        inv_pixels.append(m)
+        
+    return inv_pixels
 
-def modify_pixels(pixels):
-    pass
+
+
 
 
 
@@ -33,6 +41,6 @@ if __name__ == "__main__":
     im_path = os.path.join(root_dir, 'Reversible_Data_Hiding/misc/5.1.09.tiff')
      
     pxls = get_pixels(im_path)
-    print(pxls)
-    modify_pixels(pxls)
+    inv_pixels = inverse_pixels(pxls)
+    print(inv_pixels)
     
